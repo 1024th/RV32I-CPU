92a9dcefbe087a12286ea06e919c188ffea3a104
1024th <lintianchuan@foxmail.com>
2022-12-02 01:24:40 +0800

    add Decoder

--- src/decoder.v
+++ src/decoder.v
@@ -1,6 +1,7 @@
 `ifndef DECODER
 `define DECODER
 `include "macros.v"
+
 module Decoder (
     input wire clk,
     input wire rst,
@@ -11,16 +12,19 @@ module Decoder (
     input wire [`INST_WID] inst,
     input wire [`ADDR_WID] inst_pc,
 
-    // decode result, send to other modules
-    output wire [`ROB_POS_WID] rob_id,
-    output wire [     `OP_WID] op,
-    output wire [   `DATA_WID] rs1_val,
-    output wire [ `ROB_ID_WID] rs1_rob_id,
-    output wire [   `DATA_WID] rs2_val,
-    output wire [ `ROB_ID_WID] rs2_rob_id,
-    output wire [   `DATA_WID] imm,
-    output reg  [`REG_POS_WID] rd,
-    output wire [   `ADDR_WID] pc,
+    // issue instruction
+    output reg                issue,
+    output reg [`ROB_POS_WID] rob_pos,
+    output reg [ `OPCODE_WID] opcode,
+    output reg [ `FUNCT3_WID] funct3,
+    output reg                funct7,
+    output reg [   `DATA_WID] rs1_val,
+    output reg [ `ROB_ID_WID] rs1_rob_id,
+    output reg [   `DATA_WID] rs2_val,
+    output reg [ `ROB_ID_WID] rs2_rob_id,
+    output reg [   `DATA_WID] imm,
+    output reg [`REG_POS_WID] rd,
+    output reg [   `ADDR_WID] pc,
 
     // query in Register File
     output wire [`REG_POS_WID] reg_rs1,
@@ -29,16 +33,14 @@ module Decoder (
     output wire [`REG_POS_WID] reg_rs2,
     input  wire [   `DATA_WID] reg_val2,
     input  wire [ `ROB_ID_WID] reg_rob_id2,
-    // write to Register File
-    output reg                 reg_en,
 
     // query in Reorder Buffer
-    output wire [`ROB_ID_WID] rob_id1,
-    input  wire               rob_ready1,
-    input  wire [  `DATA_WID] rob_val1,
-    output wire [`ROB_ID_WID] rob_id2,
-    input  wire               rob_ready2,
-    input  wire [  `DATA_WID] rob_val2,
+    output wire [`ROB_POS_WID] rob_pos1,
+    input  wire                rob_ready1,
+    input  wire [   `DATA_WID] rob_val1,
+    output wire [`ROB_POS_WID] rob_pos2,
+    input  wire                rob_ready2,
+    input  wire [   `DATA_WID] rob_val2,
 
     // Reservation Station
     input  wire rs_full,
@@ -49,9 +51,8 @@ module Decoder (
     output reg  lsb_en,
 
     // Reorder Buffer
-    input  wire                rob_full,
-    input  wire [`ROB_POS_WID] nxt_rob_pos,
-    output reg                 rob_en,
+    input wire                rob_full,
+    input wire [`ROB_POS_WID] nxt_rob_pos,
 
     // handle the broadcast
     // from Reservation Station
@@ -63,7 +64,63 @@ module Decoder (
     input wire [`ROB_ID_WID] lsb_result_rob_id,
     input wire [`DATA_WID] lsb_result_val
 );
+  assign reg_rs1  = inst[19:15];
+  assign reg_rs2  = inst[24:20];
+  assign rob_pos1 = reg_rob_id1[`ROB_POS_WID];
+  assign rob_pos2 = reg_rob_id2[`ROB_POS_WID];
 
+  always @(posedge clk) begin
+    if (!inst_rdy || rs_full || lsb_full) begin
+      issue <= 0;
+    end else begin
+      issue <= 1;
+      rob_pos <= nxt_rob_pos;
+
+      opcode <= inst[6:0];
+      funct3 <= inst[14:12];
+      funct7 <= inst[30];
+      rd <= inst[11:7];
+
+      rs1_rob_id <= 0;
+      if (reg_rob_id1[4] == 0) begin
+        rs1_val <= reg_val1;
+      end else if (rob_ready1) begin
+        rs1_val <= rob_val1;
+      end else if (rs_result && reg_rob_id1 == rs_result_rob_id) begin
+        rs1_val <= rs_result_val;
+      end else if (lsb_result && reg_rob_id1 == lsb_result_rob_id) begin
+        rs1_val <= lsb_result_val;
+      end else begin
+        rs1_val <= 0;
+        rs1_rob_id <= reg_rob_id1;
+      end
+
+      rs2_rob_id <= 0;
+      if (reg_rob_id2[4] == 0) begin
+        rs2_val <= reg_val2;
+      end else if (rob_ready2) begin
+        rs2_val <= rob_val2;
+      end else if (rs_result && reg_rob_id2 == rs_result_rob_id) begin
+        rs2_val <= rs_result_val;
+      end else if (lsb_result && reg_rob_id2 == lsb_result_rob_id) begin
+        rs2_val <= lsb_result_val;
+      end else begin
+        rs2_val <= 0;
+        rs2_rob_id <= reg_rob_id2;
+      end
+
+      lsb_en <= 0;
+      rs_en  <= 0;
+      case (opcode)
+        `OPCODE_S: lsb_en <= 1;
+        `OPCODE_L: lsb_en <= 1;
+        `OPCODE_ARITHI: rs_en <= 1;
+        `OPCODE_ARITH: rs_en <= 1;
+        `OPCODE_JALR: rs_en <= 1;
+        `OPCODE_BR: rs_en <= 1;
+      endcase
+    end
+  end
 
 endmodule
 `endif  // DECODER
--- src/macros.v
+++ src/macros.v
@@ -12,10 +12,11 @@
 // rob_id = {flag, rob_pos}
 // flag: 0 = ready, 1 = renamed
 `define ROB_ID_WID 4:0
-// width of self-defined opcode
-`define OP_WID 5:0
 
 // RISC-V
+`define OPCODE_WID 6:0
+`define FUNCT3_WID 2:0
+
 `define OPCODE_L      7'b0000011
 `define OPCODE_S      7'b0100011
 `define OPCODE_ARITHI 7'b0010011
@@ -25,5 +26,51 @@
 `define OPCODE_JAL    7'b1101111
 `define OPCODE_JALR   7'b1100111
 `define OPCODE_BR     7'b1100011
+
+`define FUNCT3_ADD  3'h0
+`define FUNCT3_SUB  3'h0
+`define FUNCT3_XOR  3'h4
+`define FUNCT3_OR   3'h6
+`define FUNCT3_AND  3'h7
+`define FUNCT3_SLL  3'h1
+`define FUNCT3_SRL  3'h5
+`define FUNCT3_SRA  3'h5
+`define FUNCT3_SLT  3'h2
+`define FUNCT3_SLTU 3'h3
+
+`define FUNCT7_ADD 1'b0
+`define FUNCT7_SUB 1'b1
+`define FUNCT7_SRL 1'b0
+`define FUNCT7_SRA 1'b1
+
+`define FUNCT3_ADDI  3'h0
+`define FUNCT3_XORI  3'h4
+`define FUNCT3_ORI   3'h6
+`define FUNCT3_ANDI  3'h7
+`define FUNCT3_SLLI  3'h1
+`define FUNCT3_SRLI  3'h5
+`define FUNCT3_SRAI  3'h5
+`define FUNCT3_SLTI  3'h2
+`define FUNCT3_SLTUI 3'h3
+
+`define FUNCT7_SRLI 1'b0
+`define FUNCT7_SRAI 1'b1
+
+`define FUNCT3_LB  3'h0
+`define FUNCT3_LH  3'h1
+`define FUNCT3_LW  3'h2
+`define FUNCT3_LBU 3'h4
+`define FUNCT3_LHU 3'h5
+
+`define FUNCT3_SB 3'h0
+`define FUNCT3_SH 3'h1
+`define FUNCT3_SW 3'h2
+
+`define FUNCT3_BEQ  3'h0
+`define FUNCT3_BNE  3'h1
+`define FUNCT3_BLT  3'h4
+`define FUNCT3_BGE  3'h5
+`define FUNCT3_BLTU 3'h6
+`define FUNCT3_BGEU 3'h7
 
 `endif // MACROS
